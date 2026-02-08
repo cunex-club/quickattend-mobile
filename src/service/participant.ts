5d95c3773b55aaf14508973832be6e6d3a18d469
BadLuckZ <thanagorn8802@gmail.com>
2026-02-09 01:58:38 +0700

    feat: add api functions

---
+++ src/service/participant.ts
@@ -0,0 +1,71 @@
+import Axios from "@/utils/axios";
+
+export type CheckInStatus = "SUCCESS" | "DUPLICATE";
+
+export interface UserInformationQRCode {
+  id: string;
+  firstname_th: string | null;
+  surname_th: string | null;
+  title_th: string | null;
+  firstname_en: string | null;
+  surname_en: string | null;
+  title_en: string | null;
+  ref_id: string | null;
+  organization_th: string | null;
+  organization_en: string | null;
+  check_in_time: string;
+  status: CheckInStatus;
+  code: string;
+  profile_image_url: string | null;
+}
+
+interface ParticipantInformationQRCodeProps {
+  data: UserInformationQRCode;
+  error: null;
+  meta: null;
+}
+
+export async function getParticipantInformationQRCode(
+  qrcode: string,
+  token: string,
+  eventId: string,
+  scanned_location_lat: number,
+  scanned_location_long: number
+): Promise<UserInformationQRCode> {
+  const response = await Axios.post<ParticipantInformationQRCodeProps>(
+    `/participant/${qrcode}`,
+    {
+      event_id: eventId,
+      scanned_location_lat,
+      scanned_location_long,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data.data;
+}
+
+export async function updateParticipantCommentQRCode(
+  code: string,
+  token: string,
+  comment: string
+) {
+  const response = await Axios.put<ParticipantInformationQRCodeProps>(
+    `/participant/comment`,
+    {
+      comment,
+      one_time_code: code,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.status;
+}
