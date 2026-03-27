e38ec392dc9080404a722d73ced6347bf5ba1eef
Tonkla W. <mail@tonklaw.com>
2026-03-28 00:24:20 +0700

    fix: routing + mobile

--- src/app/[locale]/scan/[id]/page.tsx
+++ src/app/[locale]/scan/[id]/page.tsx
@@ -265,7 +265,9 @@ const ScanPage = () => {
   const startCamera = async () => {
     try {
       const mediaStream = await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: "environment" },
+        video: {
+          facingMode: { ideal: "environment" },
+        },
       });
 
       setStream(mediaStream);
