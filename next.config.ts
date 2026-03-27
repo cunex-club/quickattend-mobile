476fddec7228daae70679209a2f5cd5d70335c0b
Tonkla W. <mail@tonklaw.com>
2026-03-27 20:16:06 +0700

    ci: dockerfiles

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ import path from "path";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  /* config options here */
+  output: "standalone",
   turbopack: {
     root: path.resolve(__dirname),
   },
